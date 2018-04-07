4518abc3f39c2fc150eb30fbe43414be54d7f586
OSCAR MARTIN <elmao79@gmail.com>
2018-04-07 18:35:57 +0200

    README.md file added to sample1

--- src/lib/OMGRxJsOperators/Date.js
+++ src/lib/OMGRxJsOperators/Date.js
@@ -1,10 +1,10 @@
 
 const Operators = require('rxjs/operators');
 
-exports.greaterThan = (targetDate) => Operators.filter(item => item > targetDate);
+exports.greaterThan = targetDate => Operators.filter(item => item > targetDate);
 
-exports.greaterThanEqual = (targetDate) => Operators.filter(item => item >= targetDate);
+exports.greaterThanEqual = targetDate => Operators.filter(item => item >= targetDate);
 
-exports.lessThan = (targetDate) => Operators.filter(item => item < targetDate);
+exports.lessThan = targetDate => Operators.filter(item => item < targetDate);
 
-exports.lessThanEqual = (targetDate) => Operators.filter(item => item <= targetDate);
+exports.lessThanEqual = targetDate => Operators.filter(item => item <= targetDate);
